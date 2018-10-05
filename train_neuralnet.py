73f69af7b6daabf82a865544c06300cf9853842e
Y-Niki <yn.cell01@gmail.com>
2018-10-05 22:33:10 +0900

    modified train_neuralnet.py to calculate acc every epoch

--- train_neuralnet.py
+++ train_neuralnet.py
@@ -5,6 +5,10 @@ from two_layer_net import TwoLayerNet
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
 train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+# 1エポックあたりの繰り返し数
+iter_per_epoch = max(train_size / batch_size, 1)
 
 # ハイパーパラメータ
 iters_num = 10000
@@ -32,3 +36,11 @@ for i in range(iters_num):
     # 学習経過の記録
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
+    
+    # 1エポックごとに認識精度を計算
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + "," + str(test_acc))
